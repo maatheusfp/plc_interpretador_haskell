062cdb6e076000e8fe8016bb55e7d0410e8fd61a
Pedro Baptista <pedrolk2012@gmail.com>
2025-07-31 11:39:20 -0300

    feat: update For syntax and add Add operator in Termo data type

--- projeto/interpretador.hs
+++ projeto/interpretador.hs
@@ -10,19 +10,19 @@ type Booleano = Bool
 data Termo = Identifier Id
                 | LiteralNum Numero
                 | LiteralBool Booleano
-                | This -- Referência ao objeto atual
+                | This 
                 | If Termo Termo Termo -- Condição, then, else
                 | New Id [Termo] -- Nome da classe, argumentos
                 | While Termo Termo -- Condição, corpo
-                | For Termo Termo Termo Termo -- Inicialização, condição, atualização, corpo
+                | For Id Termo Termo Termo -- Inicialização, condição, atualização, corpo
                 | Call Termo Id [Termo]  -- objeto, método, argumentos
-                | Class Id [Id] [(Id,Termo)] [(Id, [Id], Termo)] -- Nome, superclasse, atributos, métodos
-                | Attr Id Termo -- Atribuição de atributo
-                | Assign Id Termo -- Atribuição de variável
+                | Class Id [Id] [(Id,Termo)] [(Id, [Id], Termo)] -- Nome, heranças, atributos, métodos
+                | Assign Termo Termo -- Atribuição de variável
                 | Mul Termo Termo -- Multiplicação
+                | Add Termo Termo -- adição
                 | InstanceOf Termo Id -- Verifica se um objeto é uma instância de uma classe
                 | Fun Id [Id] Termo -- Nome, parâmetros, corpo
-                | Return Termo -- Retorna um valor
+                
 
 
              
